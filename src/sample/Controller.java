c8eb983df8f4bef7b1fdcee30a711e92d78c7427
szkawin <szkawicz@gmail.com>
2017-11-09 09:46:08 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    public TableView tableview;
+    public Button button;
+    public TextField textfield;
+
+    public void handleClick(ActionEvent actionEvent) {
+    }
+}
